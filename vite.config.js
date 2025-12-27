b814207f730def82a3635ec92c94c69cad4e78e7
ASUS <nplrahma922@gmail.com>
2025-12-27 20:45:04 +0800

    Menambahkan workflow github actions

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/Akademi-Arsitek-ID/dist/',
   plugins: [
     vue(),
     tailwindcss(),
