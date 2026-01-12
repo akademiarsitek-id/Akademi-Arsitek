9f5d9bf5a18123a20371146b6525d49554d29c8c
ASUS <nplrahma922@gmail.com>
2026-01-12 14:36:03 +0800

    Install gh-pages dan setup deploy ke branch baru

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Akademi-Arsitek/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
